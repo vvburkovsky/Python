8ae6193d059c0b0652be0ae18cef2871477585ba
vvburkovsky <bvitaliy1@gmail.com>
2021-03-16 01:05:50 +0200

    add error

--- Telegram_bot/weather_current.py
+++ Telegram_bot/weather_current.py
@@ -6,19 +6,24 @@ api_key = '123456789'
 city_id ='686967' #Zhytomyr
 lang = 'ru'
 units = 'metric'
+error = 'Ошибка получения данных :-('
 
 weather_url = requests.get ('http://api.openweathermap.org/data/2.5/weather' , params = {'id' : city_id , 'units' : units, 'appid' : api_key, 'lang' : lang})
 json_item = json.loads(weather_url.text)
 
-for item in json_item:
-    city = json_item["name"]
-    sky = (json_item["weather"][0]["description"]).capitalize()
-    humidity = 'Влажность: ' + str (int (json_item["main"]["humidity"])) + '%'
-    temp = 'Температура: ' + str (int (json_item["main"]["temp"])) + ' ℃'
-    feels_like = 'Ощущается как: ' + str (int (json_item["main"]["feels_like"]))
-    date = 'Обновлено: ' + datetime.fromtimestamp(json_item["dt"]).strftime('%d-%m-%Y %H:%M:%S')
-    wind_spped = 'Ветер: ' + str (int (json_item["wind"]["speed"] * 3.6))  + ' km/h'
-
+try:
+    for item in json_item:
+        city = json_item["name"]
+        sky = (json_item["weather"][0]["description"]).capitalize()
+        humidity = 'Влажность: ' + str (int (json_item["main"]["humidity"])) + '%'
+        temp = 'Температура: ' + str (int (json_item["main"]["temp"])) + ' ℃'
+        feels_like = 'Ощущается как: ' + str (int (json_item["main"]["feels_like"]))
+        date = 'Обновлено: ' + datetime.fromtimestamp(json_item["dt"]).strftime('%d-%m-%Y %H:%M:%S')
+        wind_spped = 'Ветер: ' + str (int (json_item["wind"]["speed"] * 3.6))  + ' km/h'
+        
+        current = [city, sky, humidity, temp, feels_like, date, wind_spped]
+except:
+    current = [error]
 
 current = [city, sky, humidity, temp, feels_like, date,wind_spped]
 #print (json_item["weather"][0]["description"])
